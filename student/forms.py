536d80f96e28f08a0c28720ba1530279ea99fc30
Neeraj <u.neerajkumar5302@gmail.com>
2024-08-30 16:52:49 +0530

    Updated

--- student/forms.py
+++ student/forms.py
@@ -7,9 +7,17 @@ from .models import *
 
 
 class SignupForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
-        model = User 
-        fields = ['username', 'password1', 'password2']
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email is already in use.")
+        return email
 
 class LoginForm(forms.Form):
     username = forms.CharField()
